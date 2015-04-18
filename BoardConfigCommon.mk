1bd5d5aa4914953b6e039900b30cd963a2ada24d
Steve Kondik <steve@cyngn.com>
2015-04-18 14:15:59 -0700

    msm8974: Only dexpreopt user builds
    Change-Id: Ic56df3f507b36aad39b46b27727acadd01df7705

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -147,7 +147,7 @@ EXTENDED_FONT_FOOTPRINT := true
 
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
-  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user userdebug),true)
+  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
     endif
